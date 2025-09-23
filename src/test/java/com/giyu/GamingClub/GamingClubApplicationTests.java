a5480a2889e3a43487daca2a5dc63b1162e04e80
sudeepk18 <sudeepkharvi18@gmail.com>
2025-09-23 14:01:51 +0530

    Initial commit

---
+++ src/test/java/com/giyu/GamingClub/GamingClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giyu.GamingClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
